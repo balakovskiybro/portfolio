73693678cae2b9d8823f16c2751e77976ab31fae
balakovskiybro <balakovskiybro@gmail.com>
2024-01-24 03:06:56 +0300

    Initial commit

--- 1/js/script.js
+++ 1/js/script.js
@@ -1,24 +1,8 @@
-function customScrollFunction(id, top) {
-   document.getElementById(id).addEventListener("click", () => {
-     window.scrollTo({
-       top,
-       left: 0,
-       behavior: "smooth",
-     });
-   });
- }
- 
- customScrollFunction("portfolio", 200);
- customScrollFunction("team", 950);
- customScrollFunction("about", 1700);
- customScrollFunction("blog", 4500);
- customScrollFunction("contacts", 4850);
-
 const play = document.querySelector('.about__play');
 const video = document.querySelector('.about__video video')
 
-play.addEventListener ('click', () => {
-   video.play();
-   video.setAttribute('controls', 'controls');
-   play.classList.add('about__play--hidden');
+play.addEventListener('click', () => {
+  video.play();
+  video.setAttribute('controls', 'controls');
+  play.classList.add('about__play--hidden');
 })
